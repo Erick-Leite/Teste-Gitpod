33ced6ae4585cb982a321d098826e651ca4a02b9
Erick Leite <erickleite338@gmail.com>
2023-11-30 13:12:32 +0000

    working on the React Router framework

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,21 +2,27 @@ import './App.css';
 import Home from './components/pages/Home';
 import Projects from './components/pages/Projects';
 import Contacts from './components/pages/Contacts';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './components/pages/Home';
-import Projects from './components/pages/Projects';
-import Contacts from './components/pages/Contacts';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contacts" element={<Contacts />} />
-        </Routes>
+        <div>
+          <ul>
+            <Link to="/" >Home</Link>
+            <Link to="/projects" >Projetos</Link>
+            <Link to="/contacts" >Contatos</Link>
+          </ul>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contacts" element={<Contacts />} />
+          </Routes>
+          <footer>
+            <p>Rodapé</p>
+          </footer>
+        </div>
       </Router>
     </div>
   );
