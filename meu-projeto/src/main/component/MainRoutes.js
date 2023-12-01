2c3f7993d1cbc63d1f3778bbf73a287c59b17fad
Erick Leite <erickleite338@gmail.com>
2023-12-01 21:11:59 +0000

    update: MainRoutes.js and main_container.module.css

--- meu-projeto/src/main/component/MainRoutes.js
+++ meu-projeto/src/main/component/MainRoutes.js
@@ -3,10 +3,11 @@ import Home from "../pages/Home";
 import NewProjects from "../pages/NewProject";
 import Company from "../pages/Company";
 import Contacts from "../pages/Contacts";
+import styles from "../main_layout/main_container.module.css"
 
 function MainRoutes() {
   return (
-    <Routes>
+    <Routes className={styles.container}>
       <Route path="/" element={<Home />} />
       <Route path="/new_project" element={<NewProjects />} />
       <Route path="/company" element={<Company />} />
